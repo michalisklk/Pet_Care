2b1aba81ee25d8eb53ac452014fef83af06b980b
6espw <despo.marku@gmail.com>
2025-12-02 18:34:29 +0200

    DTO για το αποτέλεσμα της εξωτερικής υπηρεσίας

---
+++ petcare/src/main/java/gr/hua/dit/petcare/notification/dto/SendSmsResult.java
@@ -0,0 +1,10 @@
+package gr.hua.dit.petcare.notification.dto;
+
+/**
+ * Αποτέλεσμα αποστολής SMS όπως το βλέπει η εφαρμογή PetCare.
+ *
+ * Το πεδίο sent αντιστοιχεί στο αν η εξωτερική υπηρεσία (Notification Service)
+ * θεώρησε ότι η αποστολή του SMS ήταν επιτυχής ή όχι.
+ */
+public record SendSmsResult(boolean sent) {
+}
